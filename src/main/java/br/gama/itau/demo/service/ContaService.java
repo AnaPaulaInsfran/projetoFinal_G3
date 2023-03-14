5aad63e68f23a27a58ea72eb08a0e224c88a8d90
Romulera <romuloaugusto227@gmail.com>
2023-03-14 14:25:41 -0300

    metodos da classe service finalizado

--- src/main/java/br/gama/itau/demo/service/ContaService.java
+++ src/main/java/br/gama/itau/demo/service/ContaService.java
@@ -1,13 +1,14 @@
 package br.gama.itau.demo.service;
 
+import java.util.List;
 import java.util.Optional;
-
-import javax.naming.NameNotFoundException;
 
 import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.stereotype.Service;
 
+import br.gama.itau.demo.model.Cliente;
 import br.gama.itau.demo.model.Conta;
+import br.gama.itau.demo.repository.ClienteRepo;
 import br.gama.itau.demo.repository.ContaRepo;
 import lombok.RequiredArgsConstructor;
 
@@ -16,6 +17,7 @@ import lombok.RequiredArgsConstructor;
 public class ContaService {
 
     private final ContaRepo repo;
+    private final ClienteRepo clienteRepo;
 
     public Conta newConta(Conta conta) {
         if (conta.getNumeroConta() > 0) {
@@ -25,14 +27,32 @@ public class ContaService {
         return newConta;
     }
 
-    public Conta getById(long id){
+    public Conta getById(long id) throws NotFoundException {
         Optional<Conta> contaOptional = repo.findById(id);
-        if(contaOptional.isEmpty()){
+        if (contaOptional.isEmpty()) {
             throw new NotFoundException();
         }
         return contaOptional.get();
-        
+    }
 
+    public Conta updateConta(Conta conta) {
+        Optional<Conta> contaOptional = repo.findById(conta.getNumeroConta());
 
-}
+        if (contaOptional.isPresent()) {
+            conta.setNumeroConta(conta.getNumeroConta());
+            Conta updateConta = repo.save(conta);
+            return updateConta;
+        }
+        return null;
+    }
+
+    public List<Conta> getContasClientes(long idCliente) {
+        Optional<Cliente> clienteOptional = clienteRepo.findById(idCliente);
+
+        if (clienteOptional.isPresent()) {
+            return clienteOptional.get().getContas();
+        }
+        return null;
+    }
+
 }
