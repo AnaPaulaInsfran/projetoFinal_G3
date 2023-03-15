049da4b8fd6590977055dfa08a278a573f3060e8
AnaInsfran <anainsfran@gmail.com>
2023-03-15 18:01:55 -0300

    criacao teste ContaService

--- src/test/java/br/gama/itau/demo/service/ContaServiceTeste.java
+++ src/test/java/br/gama/itau/demo/service/ContaServiceTeste.java
@@ -1,22 +1,24 @@
 package br.gama.itau.demo.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentMatcher;
 import org.mockito.ArgumentMatchers;
 import org.mockito.BDDMockito;
 import org.mockito.Mockito;
 
 import br.gama.itau.demo.exceptions.NotFoundException;
+import br.gama.itau.demo.model.Cliente;
 import br.gama.itau.demo.model.Conta;
 import br.gama.itau.demo.repository.ClienteRepo;
 import br.gama.itau.demo.repository.ContaRepo;
@@ -68,4 +70,69 @@ public class ContaServiceTeste {
 
         verify(contaRepo, Mockito.times(1)).save(conta);
     }
+
+    @Test
+    void updateConta_returnContaAtualizada_whenContaValida() {
+        Optional<Conta> contaOptional = Optional.of(new Conta());
+        contaOptional.get().setNumeroConta(1L);
+        Conta conta = new Conta();
+        conta.setNumeroConta(1L);
+
+        BDDMockito.when(contaRepo.findById(conta.getNumeroConta()))
+        .thenReturn(contaOptional);
+        BDDMockito.when(contaRepo.save(conta)).thenReturn(contaOptional.get());
+
+        Conta updateConta = contaService.updateConta(conta);
+
+        assertTrue(updateConta != null);
+        assertEquals(1L, updateConta.getNumeroConta());
+
+        verify(contaRepo, Mockito.times(1))
+        .save(conta);
+    }
+
+    @Test
+    void updateConta_returnNull_whenContaInvalida() {
+        Conta conta = new Conta();
+        conta.setNumeroConta(1L);
+        
+        BDDMockito.when(contaRepo.findById(conta.getNumeroConta()))
+        .thenReturn(Optional.empty());
+
+        Conta contaNula = contaService.updateConta(conta);
+
+        assertTrue(contaNula == null);
+    }
+
+    @Test
+    void getContasClientes_returnListaContas_whenClienteValido() {
+        Conta conta = new Conta();
+        Conta conta2 = new Conta();
+        ArrayList<Conta> listaContas = new ArrayList<>();
+        listaContas.add(conta);
+        listaContas.add(conta2);
+        Optional<Cliente> clienteOptional = Optional.of(new Cliente());
+        clienteOptional.get().setContas(listaContas);
+        clienteOptional.get().setId(1L);
+
+        BDDMockito.when(clienteRepo.findById(1L)).thenReturn(clienteOptional);
+        List<Conta> contas = contaService.getContasClientes(1L);
+
+        assertEquals(2, contas.size());
+        assertFalse(contas.isEmpty());
+       // assertEquals(1L, contas.get(0).getCliente().getId()); 
+    }
+
+    @Test
+    void getContasClientes_returnNull_whenClienteInvalido() {
+
+        BDDMockito.when(clienteRepo.findById(1L)).thenReturn(Optional.empty());
+        List<Conta> contaVazia = contaService.getContasClientes(1L);
+
+       // assertEquals(0, contaVazia.size());
+        assertTrue(contaVazia.isEmpty());
+    }
+
+
+
 }
