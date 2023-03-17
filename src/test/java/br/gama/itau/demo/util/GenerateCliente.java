b76b6168b1568a819928dd81f9cf7359c945d747
Wesley Pinheiro de Carvalho <wesleypinheiroc@yahoo.com.br>
2023-03-17 17:08:29 -0300

    MovimentacaoServiceTest e teste integrado do ClienteController finalizados.

--- src/test/java/br/gama/itau/demo/util/GenerateCliente.java
+++ src/test/java/br/gama/itau/demo/util/GenerateCliente.java
@@ -2,6 +2,7 @@ package br.gama.itau.demo.util;
 import java.util.List;
 import java.util.ArrayList;
 
+import br.gama.itau.demo.dto.ClienteDTO;
 import br.gama.itau.demo.model.Cliente;
 
 public class GenerateCliente {
@@ -35,9 +36,17 @@ public class GenerateCliente {
 
     public static Cliente novoClienteSemId(){
         return Cliente.builder()
-        .cpf("34694785468")
-        .telefone("11945668321")
-        .nome("Edersson HiperBoss")
+        .cpf("34698765468")
+        .telefone("11955663321")
+        .nome("Gabriel Sr. BigBoss")
+        .build();
+    }
+
+    public static Cliente novoClienteSemId2(){
+        return Cliente.builder()
+        .cpf("34698765478")
+        .telefone("11955663521")
+        .nome("Ana MegaBoss")
         .build();
     }
 
@@ -55,7 +64,41 @@ public class GenerateCliente {
         listaCliente.add(novoCliente());
         listaCliente.add(novoCliente2());
         listaCliente.add(novoCliente3());
-        return listaCliente();
+        return listaCliente;
+    }
+
+    public static List <Cliente> listaClienteSemId(){
+        ArrayList<Cliente> listaCliente = new ArrayList<>();
+        listaCliente.add(novoClienteSemId());
+        listaCliente.add(novoClienteSemId2());
+        return listaCliente;
+    }
+
+    public static ClienteDTO novoClienteDTO(){
+        ClienteDTO clienteDTO = new ClienteDTO(        
+        Cliente.builder()
+        .cpf("34698765468")
+        .telefone("11955663321")
+        .nome("Gabriel Sr. BigBoss")
+        .build());
+        return clienteDTO;
+    }
+
+    public static ClienteDTO novoClienteDTO2(){
+        ClienteDTO clienteDTO = new ClienteDTO(        
+        Cliente.builder()
+        .cpf("34698765469")
+        .telefone("11955663322")
+        .nome("Romulo Sr. BigBoss")
+        .build());
+        return clienteDTO;
+    }
+
+    public static List <ClienteDTO> listaClienteSemIdDTO(){
+        ArrayList<ClienteDTO> listaClienteDTO = new ArrayList<>();
+        listaClienteDTO.add(novoClienteDTO());
+        listaClienteDTO.add(novoClienteDTO2());
+        return listaClienteDTO;
     }
 
 }
